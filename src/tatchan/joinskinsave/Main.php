ba917102469e6cc05652dddf323b89d1d28c6e2f
PJZ9n <38120936+PJZ9n@users.noreply.github.com>
2021-05-02 16:19:34 +0900

    コードの整形

--- src/tatchan/joinskinsave/Main.php
+++ src/tatchan/joinskinsave/Main.php
@@ -8,59 +8,55 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener
-{
-
+class Main extends PluginBase implements Listener {
     public function onEnable() {
-            $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onJoin(PlayerJoinEvent $event) {
-            $player = $event->getPlayer();
-            $skin_raw = $player->getSkin()->getSkinData();
+        $player = $event->getPlayer();
+        $skin_raw = $player->getSkin()->getSkinData();
 
-
-            $height = 64;
-            $width = 64;
-            switch (strlen($skin_raw)) {
-                case 64 * 32 * 4:
-                    $height = 32;
-                    $width = 64;
-                    break;
-                case 64 * 64 * 4:
-                    $height = 64;
-                    $width = 64;
-                    break;
-                case 128 * 64 * 4:
-                    $height = 64;
-                    $width = 128;
-                    break;
-                case 128 * 128 * 4:
-                    $height = 128;
-                    $width = 128;
-                    break;
-            }
-
-            $img = imagecreatetruecolor($width, $height);
-            imagealphablending($img, false);
-            imagesavealpha($img, true);
-
-            $index = 0;
-            for ($y = 0; $y < $height; ++$y) {
-                for ($x = 0; $x < $width; ++$x) {
-                    $list = substr($skin_raw, $index, 4);
-                    $r = ord($list[0]);
-                    $g = ord($list[1]);
-                    $b = ord($list[2]);
-                    $a = 127 - (ord($list[3]) >> 1);
-                    $index += 4;
-                    $color = imagecolorallocatealpha($img, $r, $g, $b, $a);
-                    imagesetpixel($img, $x, $y, $color);
-                }
-            }
-
-            imagepng($img, $this->getDataFolder() . $player->getName() . ".png");
-            imagedestroy($img);
+        $height = 64;
+        $width = 64;
+        switch (strlen($skin_raw)) {
+            case 64 * 32 * 4:
+                $height = 32;
+                $width = 64;
+                break;
+            case 64 * 64 * 4:
+                $height = 64;
+                $width = 64;
+                break;
+            case 128 * 64 * 4:
+                $height = 64;
+                $width = 128;
+                break;
+            case 128 * 128 * 4:
+                $height = 128;
+                $width = 128;
+                break;
         }
 
+        $img = imagecreatetruecolor($width, $height);
+        imagealphablending($img, false);
+        imagesavealpha($img, true);
+
+        $index = 0;
+        for ($y = 0; $y < $height; ++$y) {
+            for ($x = 0; $x < $width; ++$x) {
+                $list = substr($skin_raw, $index, 4);
+                $r = ord($list[0]);
+                $g = ord($list[1]);
+                $b = ord($list[2]);
+                $a = 127 - (ord($list[3]) >> 1);
+                $index += 4;
+                $color = imagecolorallocatealpha($img, $r, $g, $b, $a);
+                imagesetpixel($img, $x, $y, $color);
+            }
+        }
+
+        imagepng($img, $this->getDataFolder() . $player->getName() . ".png");
+        imagedestroy($img);
+    }
 }
